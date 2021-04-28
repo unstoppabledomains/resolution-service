a3c590bddd4246fc56a11c24e7e0296a2a890cdb
Kirill Beresnev <derainberk@gmail.com>
2021-04-28 14:05:25 +0300

    Add linters

--- src/controllers/DomainsController.test.ts
+++ src/controllers/DomainsController.test.ts
@@ -3,29 +3,33 @@ import { api } from '../api';
 import { expect } from 'chai';
 
 describe('DomainsController', () => {
-    describe('GET /domain/:domainName', () => {
-        it('should return non-minted domain', async () => {
-            const res = await supertest(api).get('/domains/unminted-long-domain.crypto').send();
-            expect(res.body).containSubset({
-                meta: {
-                    domain: 'unminted-long-domain.crypto',
-                    owner: null,
-                    resolver: null,
-                    location: 'UNMINTED'
-                },
-                records: {}
-            });
-            expect(res.status).eq(200);
-        });
+  describe('GET /domain/:domainName', () => {
+    it('should return non-minted domain', async () => {
+      const res = await supertest(api)
+        .get('/domains/unminted-long-domain.crypto')
+        .send();
+      expect(res.body).containSubset({
+        meta: {
+          domain: 'unminted-long-domain.crypto',
+          owner: null,
+          resolver: null,
+          location: 'UNMINTED',
+        },
+        records: {},
+      });
+      expect(res.status).eq(200);
     });
+  });
 
-    describe('GET /domains', () => {
-        it('should return empty response', async () => {
-            const res = await supertest(api).get('/domains?owners[]=0xC47Ef814093eCefe330604D9E81e3940ae033c9a').send();
-            expect(res.body).containSubset({
-                data: []
-            });
-            expect(res.status).eq(200);
-        });
-    })
-})
+  describe('GET /domains', () => {
+    it('should return empty response', async () => {
+      const res = await supertest(api)
+        .get('/domains?owners[]=0xC47Ef814093eCefe330604D9E81e3940ae033c9a')
+        .send();
+      expect(res.body).containSubset({
+        data: [],
+      });
+      expect(res.status).eq(200);
+    });
+  });
+});
--- src/services/WorkerRunner.ts
+++ src/services/WorkerRunner.ts
@@ -1,13 +1,15 @@
-import {Worker} from 'worker_threads';
-import {logger} from "../logger";
+import { Worker } from 'worker_threads';
+import { logger } from '../logger';
 
-export function runWorker(workerPath: string) {
-    const worker = new Worker(workerPath);
-    worker.on('error', (err) => {logger.error(err)});
-    process.on('SIGTERM', () => {
-        void worker.terminate()
-    });
-    process.on('SIGINT', () => {
-        void worker.terminate()
-    });
+export function runWorker(workerPath: string): void {
+  const worker = new Worker(workerPath);
+  worker.on('error', (err) => {
+    logger.error(err);
+  });
+  process.on('SIGTERM', () => {
+    void worker.terminate();
+  });
+  process.on('SIGINT', () => {
+    void worker.terminate();
+  });
 }
