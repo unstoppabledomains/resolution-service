6a43274db2e93e7c1de44b5523d4dc49cb63e116
sudoryan <rleinbox@gmail.com>
2021-05-10 13:02:23 -0700

    Add bugsnag config

--- src/api.ts
+++ src/api.ts
@@ -8,11 +8,24 @@ import { StatusController } from './controllers/StatusController';
 import swaggerUI from 'swagger-ui-express';
 import { validationMetadatasToSchemas } from 'class-validator-jsonschema';
 import { routingControllersToSpec } from 'routing-controllers-openapi';
+import Bugsnag from '@bugsnag/js';
+import BugsnagPluginExpress from '@bugsnag/plugin-express';
+import { env } from './env';
 
 export const api = createExpressServer({
   classTransformer: true,
   controllers: [DomainsController, StatusController],
 });
+
+if (env.APPLICATION.BUGSNAG_API_KEY) {
+  Bugsnag.start({
+    apiKey: env.APPLICATION.BUGSNAG_API_KEY,
+    plugins: [BugsnagPluginExpress],
+  });
+  const bugsnagPlugin = Bugsnag.getPlugin('express');
+  api.use(bugsnagPlugin?.requestHandler);
+  api.use(bugsnagPlugin?.errorHandler);
+}
 
 const schemas = validationMetadatasToSchemas({
   refPointerPrefix: '#/components/schemas/',
