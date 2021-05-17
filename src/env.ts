036f3c30d2167afa3e073a43a96dc45e5d25ba05
Gentelman John <jeyhunt@gmail.com>
2021-05-17 07:20:09 -0700

    linter

--- src/env.ts
+++ src/env.ts
@@ -6,14 +6,14 @@ const zilProdConfig = {
   ZNS_REGISTRY_CONTRACT: '0x9611c53be6d1b32058b2747bdececed7e1216793',
   NETWORK: 'mainnet',
   JSON_RPC_API_URL: 'https://api.zilliqa.com/',
-  WORKER_INTERVAL: 600000
+  WORKER_INTERVAL: 600000,
 };
 
 const zilDevConfig = {
   ZNS_REGISTRY_CONTRACT: '0xB925adD1d5EaF13f40efD43451bF97A22aB3d727',
   NETWORK: 'testnet',
   JSON_RPC_API_URL: 'https://dev-api.zilliqa.com/',
-  WORKER_INTERVAL: 5000
+  WORKER_INTERVAL: 5000,
 };
 
 const configMap = {
