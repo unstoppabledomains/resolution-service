aa175b262143fc2d22118887800077a4d2eb4ad9
Gentelman John <jeyhunt@gmail.com>
2021-05-17 08:36:01 -0700

    move zns config to separate file

--- src/env.ts
+++ src/env.ts
@@ -1,25 +1,6 @@
 import * as path from 'path';
+import znsConfig from './znsEnv';
 
-const enviroment = process.env.NODE_ENV as 'production' | 'test';
-
-const zilProdConfig = {
-  ZNS_REGISTRY_CONTRACT: '0x9611c53be6d1b32058b2747bdececed7e1216793',
-  NETWORK: 'mainnet',
-  JSON_RPC_API_URL: 'https://api.zilliqa.com/',
-  WORKER_INTERVAL: 600000,
-};
-
-const zilDevConfig = {
-  ZNS_REGISTRY_CONTRACT: '0xB925adD1d5EaF13f40efD43451bF97A22aB3d727',
-  NETWORK: 'testnet',
-  JSON_RPC_API_URL: 'https://dev-api.zilliqa.com/',
-  WORKER_INTERVAL: 5000,
-};
-
-const configMap = {
-  production: zilProdConfig,
-  test: zilDevConfig,
-};
 
 export const env = {
   APPLICATION: {
@@ -32,7 +13,7 @@ export const env = {
         process.env.CNS_REGISTRY_EVENTS_STARTING_BLOCK || 9080000
       ),
     },
-    ZILLIQA: configMap[enviroment],
+    ZILLIQA: znsConfig,
     NEW_RELIC_LICENSE_KEY: process.env.NEW_RELIC_LICENSE_KEY || '',
     NEW_RELIC_APP_NAME: process.env.NEW_RELIC_APP_NAME || '',
     BUGSNAG_API_KEY: process.env.BUGSNAG_API_KEY || '',
