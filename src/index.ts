8d001a284fcc4588d0321fb4ba65951ca5080373
sudoryan <rleinbox@gmail.com>
2021-05-18 11:39:41 -0700

    Remove worker_threads

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,26 @@
 import 'reflect-metadata';
-import { runWorker } from './services/WorkerRunner';
 import { api } from './api';
 import { env } from './env';
-import path from 'path';
 import { logger } from './logger';
 import('newrelic');
 const runningMode = env.APPLICATION.RUNNING_MODE;
 import connect from './database/connect';
+import CnsUpdater from './workers/CnsUpdater';
+import ZnsUpdater from './workers/ZnsUpdater';
 
 connect().then(() => {
   if (runningMode.includes('CNS_WORKER')) {
-    runWorker(path.resolve(__dirname, 'workers/CnsUpdater.import.js'));
+    CnsUpdater();
     logger.info('CNS worker is enabled and running');
   }
 
   if (runningMode.includes('ZNS_WORKER')) {
-    runWorker(path.resolve(__dirname, 'workers/ZnsUpdater.import.js'));
+    ZnsUpdater();
     logger.info(`ZNS worker is enabled and running`);
   }
 
   if (runningMode.includes('API')) {
-    const server = api.listen(env.APPLICATION.PORT);
-    process.on('SIGTERM', () => server.close());
-    process.on('SIGINT', () => server.close());
+    api.listen(env.APPLICATION.PORT);
     logger.info(`API is enabled and running on port ${env.APPLICATION.PORT}`);
   }
 });
--- src/workers/ZnsUpdater.ts
+++ src/workers/ZnsUpdater.ts
@@ -1,7 +1,9 @@
 import { logger } from '../logger';
 import { setIntervalAsync } from 'set-interval-async/dynamic';
 
-setIntervalAsync(async () => {
-  // todo Zns polling logic is here
-  logger.info('ZnsUpdater is pulling updates from Zilliqa');
-}, 5000); // todo adjust intervals up to 10 minutes
+export default (): void => {
+  setIntervalAsync(async () => {
+    // todo Zns polling logic is here
+    logger.info('ZnsUpdater is pulling updates from Zilliqa');
+  }, 5000); // todo adjust intervals up to 10 minutes
+};
