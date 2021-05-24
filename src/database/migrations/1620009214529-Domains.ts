7e37da0178b5bdf292961adb017fc6adebdd471e
Gentelman John <jeyhunt@gmail.com>
2021-05-23 23:19:30 -0700

    separate query for root domains

--- src/database/migrations/1620009214529-Domains.ts
+++ src/database/migrations/1620009214529-Domains.ts
@@ -23,13 +23,6 @@ export class Domains1620009214529 implements MigrationInterface {
     await queryRunner.query(
       `ALTER TABLE "domains" ADD CONSTRAINT "FK_0098b266e6691783004667114f6" FOREIGN KEY ("parent_id") REFERENCES "domains"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
     );
-    await queryRunner.query(
-      `INSERT INTO "domains" (name, node, location)
-      VALUES ('zil', '0x9915d0456b878862e822e2361da37232f626a2e47505c8795134a95d36138ed3', 'ZNS'),
-             ('crypto', '0x0f4a10a4f46c288cea365fcf45cccf0e9d901b945b9829ccdb54c10dc3cb7a6f', 'CNS')
-      ON CONFLICT (name) DO NOTHING
-   `,
-    );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
