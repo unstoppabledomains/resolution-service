c797285ed874bb8d7f04eebd267590c936d180c7
DancingAxolotl <5020980+DancingAxolotl@users.noreply.github.com>
2021-08-09 11:59:03 +0100

    Run prettier again

--- src/middleware/ApiKeyAuthMiddleware.ts
+++ src/middleware/ApiKeyAuthMiddleware.ts
@@ -8,8 +8,7 @@ import { ApiKey } from '../models';
 
 @Middleware({ type: 'before' })
 export class ApiKeyAuthMiddleware implements ExpressMiddlewareInterface {
-  static BearerUuidRegex =
-    /^Bearer ([0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12})$/i;
+  static BearerUuidRegex = /^Bearer ([0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12})$/i;
 
   async use(req: Request, res: Response, next: NextFunction) {
     const providedKey = req.headers.authorization?.match(
