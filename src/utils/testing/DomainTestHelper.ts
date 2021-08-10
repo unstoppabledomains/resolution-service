838c1db9bba3de18558af43e04c3871ee596e85b
Gentelman John <jeyhunt@gmail.com>
2021-08-10 04:36:02 -0700

    add domain test helper

---
+++ src/utils/testing/DomainTestHelper.ts
@@ -0,0 +1,20 @@
+import { Domain } from '../../models';
+import { Attributes } from '../../types/common';
+
+export class DomainTestHelper {
+  static async createTestDomain(options: Attributes<Domain>): Promise<Domain> {
+    return Domain.findOrCreate({
+      ...options,
+      name: options.name ?? 'testdomain.crypto',
+      ownerAddress:
+        options.ownerAddress ?? '0x8aaD44321A86b170879d7A244c1e8d360c99DdA8',
+      node:
+        options.node ??
+        '0x77694b72888ab3b13c9c7eb4f343045d3820c1202c1765255b896280a8bc7b55',
+      location: options.location ?? 'CNS',
+      resolution: options.resolution ?? {},
+      resolver:
+        options.resolver ?? '0xb66DcE2DA6afAAa98F2013446dBCB0f4B0ab2842',
+    });
+  }
+}
