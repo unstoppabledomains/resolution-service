208013efa228c85f73542d740cf510c70d21a360
sudoryan <rleinbox@gmail.com>
2021-12-20 00:46:35 -0600

    increase app_start_timeout_sec

--- create-yaml.sh
+++ create-yaml.sh
@@ -23,7 +23,7 @@ readiness_check:
   timeout_sec: 4
   failure_threshold: 2
   success_threshold: 2
-  app_start_timeout_sec: 300
+  app_start_timeout_sec: 1200
 
 env_variables:
   POLYGON_CONFIRMATION_BLOCKS: ${POLYGON_CONFIRMATION_BLOCKS}
